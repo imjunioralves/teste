d17300bd6a028b7053cce1e4bc97f501ed680b63
imjunioralves <imjunioralves@gmail.com>
2025-08-06 23:23:56 -0300

    feito até o exercicio 3

--- Testando-codigo/index.js
+++ Testando-codigo/index.js
@@ -9,3 +9,22 @@ function clicarNoBotao2(){
 function clicarNoBotao3(){
     alert("você clicou no botão 3")
 }
+
+function clicarNoBotao4(){
+    let digitarNome = prompt("Qual é o seu nome ?")
+
+    if(digitarNome){
+        let digitouIdade = prompt("Olá " + digitarNome + ", Qual a sua idade ?")
+    
+
+    alert("Acabei de conhecer " + digitarNome + "que tem " + digitouIdade + " anos")
+}}
+
+function clicarNoBotao5(){
+    let numero = prompt("digite um número inteiro qualquer")
+
+    if(numero){
+        let antecessor = numero - 1
+        let sucessor = Number(numero) + 1
+        alert("Antes de " + numero + ", temos o número " + antecessor + " " + "depois do " + numero + ", temos o número " + sucessor)
+    }}
