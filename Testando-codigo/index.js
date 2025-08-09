767e48ce87decb5324ab155bf6be5ae1b032236b
imjunioralves <imjunioralves@gmail.com>
2025-08-09 14:35:35 -0300

    atualizado até o exercicio 4

--- Testando-codigo/index.js
+++ Testando-codigo/index.js
@@ -28,3 +28,14 @@ function clicarNoBotao5(){
         let sucessor = Number(numero) + 1
         alert("Antes de " + numero + ", temos o número " + antecessor + " " + "depois do " + numero + ", temos o número " + sucessor)
     }}
+
+function calculadoraDeTroco(){
+    let produto = prompt("Qual produto você está comprando?")
+    let valorDoProduto = Number(prompt(`Quanto custa ${produto} que você está comprando ?`))
+    let valorParaPagar = Number(prompt(`Qual foi o valor que você deu para para ${produto}?`))
+    let troco =  valorParaPagar - valorDoProduto
+    alert(`Você comprou ${produto} que custou R$${valorDoProduto.toFixed(2)}. 
+Deu R$${valorParaPagar.toFixed(2)} em dinheiro e vai receber R$${troco.toFixed(2)} de troco.`);
+
+
+}
