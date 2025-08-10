28399d705a380911814b610f861a92771a4ba322
imjunioralves <imjunioralves@gmail.com>
2025-08-10 10:54:55 -0300

    desafio n5

--- Testando-codigo/index.js
+++ Testando-codigo/index.js
@@ -26,7 +26,8 @@ function clicarNoBotao5(){
     if(numero){
         let antecessor = numero - 1
         let sucessor = Number(numero) + 1
-        alert("Antes de " + numero + ", temos o número " + antecessor + " " + "depois do " + numero + ", temos o número " + sucessor)
+        alert(`Antes de ${numero}, temos o número ${antecessor}.
+depois do ${numero}, temos o número ${sucessor}`)
     }}
 
 function calculadoraDeTroco(){
@@ -36,6 +37,24 @@ function calculadoraDeTroco(){
     let troco =  valorParaPagar - valorDoProduto
     alert(`Você comprou ${produto} que custou R$${valorDoProduto.toFixed(2)}. 
 Deu R$${valorParaPagar.toFixed(2)} em dinheiro e vai receber R$${troco.toFixed(2)} de troco.`);
+}
 
+function conversorDeMedidas(){
+    let distancia = Number(prompt("Digite uma distancia em metros (m)"))
+    let resultado = window.document.getElementById('medidaConvertida')
 
+    let km = distancia / 1000
+    let hm = distancia / 100
+    let dam = distancia / 10
+    let dm = distancia * 10
+    let cm = distancia * 100
+    let mm = distancia *1000
+
+    resultado.innerHTML = `<p><b>A distância de ${distancia} metros, corresponde a...</b></p>`
+    resultado.innerHTML += `<p>${km} quilômetros (km)</p>`
+    resultado.innerHTML += `<p>${hm} hectômetros (hm)</p>`
+    resultado.innerHTML += `<p>${dam} decâmetros (dam)</p>`
+    resultado.innerHTML += `<p>${dm} decímetros (dm)</p>`
+    resultado.innerHTML += `<p>${cm} centímetros (cm)</p>`
+    resultado.innerHTML += `<p>${mm} milímetros (mm)</p>`
 }
